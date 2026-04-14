6e6906da5c96beed68f20b61cbe237ac771d91e8
AyeshaMuskan-a <ayeshamuskan.w@gmail.com>
2026-04-14 09:11:57 +0530

    Update credit_rike_assessment_model_queries.sql

--- credit_rike_assessment_model_queries.sql
+++ credit_rike_assessment_model_queries.sql
@@ -64,24 +64,10 @@ SELECT fraud_flag, COUNT(*)
 FROM credit_risk_dataset
 GROUP BY fraud_flag;
 
--- Window Function (Ranking by Income)
-SELECT customer_id, annual_income,
-RANK() OVER (ORDER BY annual_income DESC) AS income_rank
-FROM credit_risk_dataset;
-
 -- Top 5 High Income Customers
 SELECT * FROM credit_risk_dataset
 ORDER BY annual_income DESC
 LIMIT 5;
-
--- Running Total (Window Function)
-SELECT customer_id, annual_income,
-SUM(annual_income) OVER (ORDER BY annual_income) AS running_total
-FROM credit_risk_dataset;
-
--- Default Rate/ RISK PERCENTAGE
-SELECT (SUM(credit_risk) * 100.0 / COUNT(*)) AS default_rate_percentage
-FROM credit_risk_dataset;
 
 -- Top Risky Customers (High Debt + Low Score)
 SELECT * 
@@ -89,18 +75,5 @@ FROM credit_risk_dataset
 WHERE credit_score < 600 
 AND total_outstanding_debt > 5000;
 
--- filter customers earning above average income
-SELECT *
-FROM credit_risk_dataset
-WHERE annual_income > (
-    SELECT AVG(annual_income) FROM credit_risk_dataset
-);
-
--- Average Credit Score by Employment
-SELECT employment_status, AVG(credit_score) AS avg_score
-FROM credit_risk_dataset
-GROUP BY employment_status;
-
 -- Shows category understanding of employment_status
 SELECT DISTINCT employment_status FROM credit_risk_dataset;
-
